58cf637028c8ff9241a8d36e99de61ed073eccbf
Eduardo Matias <eduuumattias@gmail.com>
2025-11-28 20:34:33 -0300

    mudança mobile responsivo

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -41,7 +41,6 @@ const Home = () => {
 
     const fetchProducts = async () => {
         try {
-            // Ajuste a porta (ex: 3000, 5000) conforme seu server.js está rodando
             const response = await fetch('http://localhost:3000/api/product'); 
             const data = await response.json();
             setProducts(data);
